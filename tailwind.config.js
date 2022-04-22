a1d51e2cc5993e11b4efcc568f086ee3c0689119
Trihartonoo <Triihartono29@gmail.com>
2022-04-22 14:32:28 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      height: {
+        110: '110vh',
+      },
+      lineHeight: {
+        75: '75px',
+      },
+      fontFamily: {
+        dmsans: ['DM Sans'],
+        serif: ['sans-serif'],
+      },
+    },
+  },
+};
